33a9cb1ce10e2014c243e24cf69e9ee78bb81485
Brahmadutta02 <87225975+Brahmadutta02@users.noreply.github.com>
2022-07-04 22:11:39 +0530

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,28 @@
+// Hello World
+
+console.log("Hello World"); // Hello World
+
+
+// Printing input
+
+var input = "This is the first and only line of input string";
+console.log(input); //This is the first and only line of input string
+
+
+
+//Declaring variable
+
+var newVariable = "AlmaBetter";
+console.log(newVariable);
+var newVariable;
+newVariable = "AlmaBetter";
+
+
+
+//Type Conversion
+
+let number = "777";
+let integer = 8;
+let integer2 = 785;
+console.log(number+integer);
+console.log(integer2);
